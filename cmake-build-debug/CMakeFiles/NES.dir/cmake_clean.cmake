0ec673b5208a82167e2c69231df12d64fe8e423c
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-21 20:12:54 +0100

    dodan gitignore

---
+++ cmake-build-debug/CMakeFiles/NES.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NES.dir/main.cpp.o"
+  "NES"
+  "NES.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NES.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
