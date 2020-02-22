2e87b75716aec4b8442180f9e65707d897916a45
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-22 19:44:00 +0100

    promjena interfejsa klase Operation

--- AddressingMode.h
+++ AddressingMode.h
@@ -14,18 +14,18 @@ class AddressingMode {
     //klasa koja prikazuje sve moguće načine adresiranja
 
 public:
-    static uint8_t IMP(cpu6502 *cpu);
-    static uint8_t IMM(cpu6502 *cpu);
-    static uint8_t ZP0(cpu6502 *cpu);
-    static uint8_t ZPX(cpu6502 *cpu);
-    static uint8_t ZPY(cpu6502 *cpu);
-    static uint8_t REL(cpu6502 *cpu);
-    static uint8_t ABS(cpu6502 *cpu);
-    static uint8_t ABX(cpu6502 *cpu);
-    static uint8_t ABY(cpu6502 *cpu);
-    static uint8_t IND(cpu6502 *cpu);
-    static uint8_t IZX(cpu6502 *cpu);
-    static uint8_t IZY(cpu6502 *cpu);
+    static uint8_t IMP(cpu6502 &cpu);
+    static uint8_t IMM(cpu6502 &cpu);
+    static uint8_t ZP0(cpu6502 &cpu);
+    static uint8_t ZPX(cpu6502 &cpu);
+    static uint8_t ZPY(cpu6502 &cpu);
+    static uint8_t REL(cpu6502 &cpu);
+    static uint8_t ABS(cpu6502 &cpu);
+    static uint8_t ABX(cpu6502 &cpu);
+    static uint8_t ABY(cpu6502 &cpu);
+    static uint8_t IND(cpu6502 &cpu);
+    static uint8_t IZX(cpu6502 &cpu);
+    static uint8_t IZY(cpu6502 &cpu);
 };
 
 
