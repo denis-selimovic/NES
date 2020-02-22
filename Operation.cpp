7698378b8857393c963809692be5bef676e11ad4
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-22 20:23:18 +0100

    dodane implementacije operacija

--- Operation.cpp
+++ Operation.cpp
@@ -7,3 +7,227 @@
 uint8_t Operation::BRK(cpu6502 &cpu) {
     return 0;
 }
+
+uint8_t Operation::ADC(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::AND(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::ASL(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::BCC(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::BCS(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::BEQ(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::BIT(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::BMI(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::BNE(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::BPL(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::BVC(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::BVS(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::CLC(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::CLD(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::CLI(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::CLV(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::CMP(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::CPX(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::CPY(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::DEC(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::DEX(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::DEY(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::EOR(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::INC(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::INX(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::INY(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::JMP(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::JSR(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::LDA(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::LDX(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::LDY(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::LSR(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::NOP(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::ORA(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::PHA(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::PHP(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::PLA(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::PLP(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::ROL(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::ROR(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::RTI(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::RTS(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::SBC(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::SEC(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::SED(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::SEI(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::STA(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::STX(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::STY(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::TAX(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::TAY(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::TSX(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::TXA(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::TXS(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::TYA(cpu6502 &cpu) {
+    return 0;
+}
+
+uint8_t Operation::XXX(cpu6502 &cpu) {
+    return 0;
+}
