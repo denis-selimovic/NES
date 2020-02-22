9041d3118b6e91b19a494cf9c0631638e1b8740a
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-22 12:04:09 +0100

    funkcije za pisanje i citanje memorije

--- Bus.cpp
+++ Bus.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Bus.h"
+
+uint8_t Bus::readMemory(uint16_t address) {
+    if(address >= 0x0000 && address <= 0xFFFF) return RAM[address];
+    return 0;
+}
+
+void Bus::writeMemory(uint16_t address, uint8_t data) {
+    if(address >= 0x0000 && address <= 0xFFFF) RAM[address] = data;
+}
