dc6c767fd9aaf9ab26d4423f82ecbcf1e4458b1e
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-24 18:41:24 +0100

    implementacija funkcije za citanje

--- ppu2C02.cpp
+++ ppu2C02.cpp
@@ -13,6 +13,13 @@ void ppu2C02::writeCPUMemory(uint16_t address, uint8_t data) {
 }
 
 uint8_t ppu2C02::readPPUMemory(uint16_t address) {
+    //mapiramo adresu na opseg 0x0000 do 0x3fff
+    address &= 0x3FFFu;
+    if(address >= 0x0000 && address <= 0x1FFF) return pattern_table[(address & 0x1000u) >> 12u][address & 0x0FFFu];
+    else if(address >= 0x2000 && address <= 0x3EFF) {
+        address &= 0xFFFu;
+
+    }
     return 0;
 }
 
