6c1240cca1e6bfe2432c4d98cb31aa8293476036
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-24 17:13:51 +0100

    ispravke pri upisu u registre ppu

--- Bus.cpp
+++ Bus.cpp
@@ -6,13 +6,13 @@
 
 uint8_t Bus::readCPUMemory(uint16_t address) {
     if(address >= 0x0000 && address <= 0x1FFF) return RAM[address & 0x07FFu];
-    else if(address >= 0x2000 && address <= 0x3FFF) return ppu->readCPUMemory(address);
+    else if(address >= 0x2000 && address <= 0x3FFF) return ppu->readCPUMemory(address & 0x0007u);
     return 0;
 }
 
 void Bus::writeCPUMemory(uint16_t address, uint8_t data) {
     if(address >= 0x0000 && address <= 0x01FF) RAM[address & 0x07FFu] = data;
-    else if (address >= 0x2000 && address <= 0x3FFF) ppu->writeCPUMemory(address, data);
+    else if (address >= 0x2000 && address <= 0x3FFF) ppu->writeCPUMemory(address & 0x0007u, data);
 }
 
 void Bus::connectPPU(ppu2C02 *ppu) {
