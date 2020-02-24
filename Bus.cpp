6d578f559b41f73cf4f7de4fa9d8a795d71b8d9c
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-24 17:08:51 +0100

    izmjene pri čitanju memorije cpu

--- Bus.cpp
+++ Bus.cpp
@@ -5,10 +5,12 @@
 #include "Bus.h"
 
 uint8_t Bus::readCPUMemory(uint16_t address) {
-    if(address >= 0x0000 && address <= 0x07FF) return RAM[address & 0x01FFu];
+    if(address >= 0x0000 && address <= 0x1FFF) return RAM[address & 0x07FFu];
+    else if(address >= 0x2000 && address <= 0x3FFF) return ppu->readCPUMemory(address);
     return 0;
 }
 
 void Bus::writeCPUMemory(uint16_t address, uint8_t data) {
-    if(address >= 0x0000 && address <= 0x07FF) RAM[address & 0x01FFu] = data;
+    if(address >= 0x0000 && address <= 0x01FF) RAM[address & 0x07FFu] = data;
+    else if (address >= 0x2000 && address <= 0x3FFF) ppu->writeCPUMemory(address, data);
 }
