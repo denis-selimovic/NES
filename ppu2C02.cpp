25a121fcc9363dd2f50f1cb08d190348d19d80ac
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-24 17:09:35 +0100

    dodana klasa za ppu

---
+++ ppu2C02.cpp
@@ -0,0 +1,5 @@
+//
+// Created by denis on 24/02/2020.
+//
+
+#include "ppu2C02.h"
---
+++ ppu2C02.h
@@ -0,0 +1,14 @@
+//
+// Created by denis on 24/02/2020.
+//
+
+#ifndef NES_PPU2C02_H
+#define NES_PPU2C02_H
+
+
+class ppu2C02 {
+
+};
+
+
+#endif //NES_PPU2C02_H
