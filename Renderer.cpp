813c06eb279d467cc99d436ce43b19a0368a113a
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-25 17:21:45 +0100

    dodana klasa Renderer

---
+++ Renderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by denis on 25/02/2020.
+//
+
+#include "Renderer.h"
---
+++ Renderer.h
@@ -0,0 +1,15 @@
+//
+// Created by denis on 25/02/2020.
+//
+
+#ifndef NES_RENDERER_H
+#define NES_RENDERER_H
+
+#include <SDL2/SDL.h>
+
+class Renderer {
+
+};
+
+
+#endif //NES_RENDERER_H
