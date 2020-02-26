042e62a2fde668ef1df453c4fcddc40a8ebdad4e
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-26 18:19:09 +0100

    dodane funkcije iz apstraktne klase

--- Mapper000.cpp
+++ Mapper000.cpp
@@ -5,3 +5,23 @@
 #include "Mapper000.h"
 
 Mapper000::Mapper000(uint8_t pgr_banks, uint8_t chr_banks) : Mapper(pgr_banks, chr_banks) {}
+
+void Mapper000::reset() {
+
+}
+
+bool Mapper000::writeCPUMemory(uint16_t address, uint32_t &mapped_address) {
+    return false;
+}
+
+bool Mapper000::readCPUMemory(uint16_t address, uint32_t &mapped_address) {
+    return false;
+}
+
+bool Mapper000::writePPUMemory(uint16_t address, uint32_t &mapped_address) {
+    return false;
+}
+
+bool Mapper000::readPPUMemory(uint16_t address, uint32_t &mapped_address) {
+    return false;
+}
--- Mapper000.h
+++ Mapper000.h
@@ -12,6 +12,12 @@ class Mapper000: public Mapper{
 
 public:
     Mapper000(uint8_t pgr_banks, uint8_t chr_banks);
+
+    void reset() override;
+    bool writeCPUMemory(uint16_t address, uint32_t &mapped_address) override;
+    bool readCPUMemory(uint16_t address, uint32_t &mapped_address) override;
+    bool writePPUMemory(uint16_t address, uint32_t &mapped_address) override;
+    bool readPPUMemory(uint16_t address, uint32_t &mapped_address) override;
 };
 
 
