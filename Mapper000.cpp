f19eb3dcce3b4477608b2d59398b540d33e4ccee
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-26 18:31:43 +0100

    dodane funkcije za mapiranje ppu memorije pomoću mappera

--- Mapper000.cpp
+++ Mapper000.cpp
@@ -7,7 +7,7 @@
 Mapper000::Mapper000(uint8_t pgr_banks, uint8_t chr_banks) : Mapper(pgr_banks, chr_banks) {}
 
 void Mapper000::reset() {
-
+    // ovaj mapper ne radi ništa u reset funkciji
 }
 
 bool Mapper000::writeCPUMemory(uint16_t address, uint32_t &mapped_address) {
@@ -37,9 +37,23 @@ bool Mapper000::readCPUMemory(uint16_t address, uint32_t &mapped_address) {
 }
 
 bool Mapper000::writePPUMemory(uint16_t address, uint32_t &mapped_address) {
+    // što se tiče ppu sabirnice adresa je što se ovog mappera tiče samo proslijeđena tj nema mapiranja
+    // opseg sa ppu sabirnice od 0x0000 do 0x1FFF se mapira u isti taj opseg
+    // pošto se radi o CHR-ROM upis nije moguć pa nema ni mapiranja
+    // mapiranje je jedino moguće ako je to ustvari CHR-RAM tj kad je broj bankova 0
+    if(address >= 0x0000 && address <= 0x1FFF && CHR_banks == 0) {
+        mapped_address = address;
+        return true;
+    }
     return false;
 }
 
 bool Mapper000::readPPUMemory(uint16_t address, uint32_t &mapped_address) {
+    // što se tiče ppu sabirnice adresa je što se ovog mappera tiče samo proslijeđena tj nema mapiranja
+    // opseg sa ppu sabirnice od 0x0000 do 0x1FFF se mapira u isti taj opseg
+    if(address >= 0x0000 && address <= 0x1FFF && CHR_banks == 0) {
+        mapped_address = address;
+        return true;
+    }
     return false;
 }
