1449a6f971218bd3092cdb71c23510288ee7f65c
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-26 17:15:17 +0100

    dodana struktura NESHeader fajla

--- GamePak.h
+++ GamePak.h
@@ -6,11 +6,33 @@
 #define NES_GAMEPAK_H
 
 #include <string>
+#include <vector>
 
 class GamePak {
 public:
     enum MIRRORING {HORIZONTAL, VERTICAL} mirroring = HORIZONTAL;
 
+
+    //GamePak se sastoji PRG (programske memorije PRG-ROM) i CHR(memorija sprite-ova CHR-ROM)
+    //njih učitamo iz .nes datoteke
+    std::vector<uint8_t> PRG;
+    std::vector<uint8_t> CHR;
+
+
+    //pomoćna struktura za učitavanje header-a .nes fajla
+    //na osnovu ovoga saznajemo informacije o vrsti .nes fajla
+    //možemo odrediti kapacitet PRG i CHR memorije
+    struct NESHeader {
+        uint8_t header[4];
+        uint8_t prg_rom_size;
+        uint8_t chr_rom_size;
+        uint8_t flags_mirroring_trainer;
+        uint8_t flags_playchoice;
+        uint8_t flags_prg_ram_size;
+        uint8_t tv_system;
+        uint8_t prg_ram_presence;
+        uint8_t unused[5];
+    };
 public:
     GamePak(const std::string &game);
 };
