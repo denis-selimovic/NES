efdf51d7d23727de4eef1e65256195b9bb90d4e4
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-26 18:05:45 +0100

    dodan interfejs klase Mapper

--- Mapper.h
+++ Mapper.h
@@ -17,6 +17,15 @@ protected:
 public:
     Mapper(uint8_t prg, uint8_t chr);
     virtual ~Mapper();
+    virtual void reset() = 0;
+
+    //funkcije za mapiranje adresa CPU
+    virtual void writeCPUMemory(uint16_t address, uint32_t &mapped_address) = 0;
+    virtual void readCPUMemory(uint16_t address, uint32_t &mapped_address) = 0;
+
+    //funkcije za mapiranje adresa PPU
+    virtual void writePPUMemory(uint16_t address, uint32_t &mapped_address) = 0;
+    virtual void readPPUMemory(uint16_t address, uint32_t &mapped_address) = 0;
 };
 
 
