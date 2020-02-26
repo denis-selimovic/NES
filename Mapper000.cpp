fa7650bde3f78d6163305de42d22ea55d7478712
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-26 18:28:15 +0100

    funkcije za mapiranje adrese sa cpu sabirnice

--- Mapper000.cpp
+++ Mapper000.cpp
@@ -11,10 +11,28 @@ void Mapper000::reset() {
 }
 
 bool Mapper000::writeCPUMemory(uint16_t address, uint32_t &mapped_address) {
+    // ova funkcija mapira adresu sa CPU sabirnice u adresu koja postoji na GamePak-u
+    // na sabirnici za CPU adrese od 0x8000 do 0xFFFF su rezervisane za pristup GamePak-u
+    // te adrese se onda preko mapera mapiraju u opseg adresa koje obuhvata PRG-ROM
+    // ako je broj bankova 1 adrese iz opsega CPU se mapiraju u 0x0000-0x3FFF
+    // ako je broj bankova veći od 1 adrese se mapiraju u 0x0000-0x7FFFF opseg PRG-ROM
+    if(address >= 0x8000 && address <= 0xFFFF) {
+        mapped_address = address & unsigned(PRG_banks > 1 ? 0x7FFF : 0x3FFF);
+        return true;
+    }
     return false;
 }
 
 bool Mapper000::readCPUMemory(uint16_t address, uint32_t &mapped_address) {
+    // ova funkcija mapira adresu sa CPU sabirnice u adresu koja postoji na GamePak-u
+    // na sabirnici za CPU adrese od 0x8000 do 0xFFFF su rezervisane za pristup GamePak-u
+    // te adrese se onda preko mapera mapiraju u opseg adresa koje obuhvata PRG-ROM
+    // ako je broj bankova 1 adrese iz opsega CPU se mapiraju u 0x0000-0x3FFF
+    // ako je broj bankova veći od 1 adrese se mapiraju u 0x0000-0x7FFFF opseg PRG-ROM
+    if(address >= 0x8000 && address <= 0xFFFF) {
+        mapped_address = address & unsigned(PRG_banks > 1 ? 0x7FFF : 0x3FFF);
+        return true;
+    }
     return false;
 }
 
