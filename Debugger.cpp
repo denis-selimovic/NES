c7c0389baaa2d2922542ef31354a4d9afb691ec1
dselimovic1 <dselimovic1@etf.unsa.ba>
2020-02-28 19:33:43 +0100

    dodan debugger

---
+++ Debugger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by denis on 28/02/2020.
+//
+
+#include "Debugger.h"
---
+++ Debugger.h
@@ -0,0 +1,14 @@
+//
+// Created by denis on 28/02/2020.
+//
+
+#ifndef NES_DEBUGGER_H
+#define NES_DEBUGGER_H
+
+
+class Debugger {
+
+};
+
+
+#endif //NES_DEBUGGER_H
